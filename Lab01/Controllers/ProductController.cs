84e09e5354ce3eb888704afad08cfaf53ff20af5
Hien Luong <hyhien@gmail.com>
2023-06-21 10:41:35 +0700

    GetAllProduct

---
+++ Lab01/Controllers/ProductController.cs
@@ -0,0 +1,20 @@
+﻿using Lab01.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Lab01.Controllers
+{
+    public class ProductController : Controller
+    {
+        static List<Product> products = new List<Product>()
+        {
+            new Product{Id = 1, Name = "IPhone 14", Price = 27999},
+            new Product{Id = 2, Name = "IPhone 15", Price = 29999}
+        };
+
+        public IActionResult Index()
+        {
+            // gửi mảng sản phẩm qua View để hiển thị
+            return View(products);
+        }
+    }
+}
---
+++ Lab01/Models/Product.cs
@@ -0,0 +1,10 @@
+﻿namespace Lab01.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public double Price { get; set; }
+
+    }
+}
