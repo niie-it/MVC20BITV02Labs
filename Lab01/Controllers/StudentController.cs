9bd05d9d314b1c3c1084f2d927e075671f8fe7fd
Hien Luong <hyhien@gmail.com>
2023-06-28 11:01:19 +0700

    Read, Write JSON file

---
+++ Lab01/Controllers/StudentController.cs
@@ -0,0 +1,46 @@
+﻿using Lab01.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Text.Json;
+
+namespace Lab01.Controllers
+{
+    public class StudentController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        const string JSON_FILE = "Student.json";
+        const string TEXT_FILE = "Student.txt";
+
+        [HttpPost]
+        public IActionResult Manage(Student sv, string Save)
+        {
+            if (Save == "Save to JSON file")
+            {
+                var filePath = Path.Combine(Directory.GetCurrentDirectory(), "Data", JSON_FILE);
+
+                var jsonContent = JsonSerializer.Serialize(sv);
+
+                System.IO.File.WriteAllText(filePath, jsonContent);
+            }
+            return View("Index", sv);
+        }
+
+        public IActionResult ReadData(string filetype)
+        {
+            var sv = new Student();
+
+            if (filetype == "JSON")
+            {
+                var filePath = Path.Combine(Directory.GetCurrentDirectory(), "Data", JSON_FILE);
+                var fileContent = System.IO.File.ReadAllText(filePath);
+
+                sv = JsonSerializer.Deserialize<Student>(fileContent);
+            }
+
+            return View("Index", sv);
+        }
+    }
+}
---
+++ Lab01/Models/Student.cs
@@ -0,0 +1,20 @@
+﻿namespace Lab01.Models
+{
+    public class Student
+    {
+        public int StudentId { get; set; }
+        public string FullName { get; set; }
+        public double Mark { get; set; }
+        public string Grade
+        {
+            get
+            {
+                if (Mark >= 8.5) return "A";
+                if (Mark >= 7) return "B";
+                if (Mark >= 5.5) return "C";
+                if (Mark >= 4) return "D";
+                return "F";
+            }
+        }
+    }
+}
