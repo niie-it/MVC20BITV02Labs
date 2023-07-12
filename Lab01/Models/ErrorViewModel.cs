c10fc92f0814964999cd82dcd6365759abf8d4e4
Hien Luong <hyhien@gmail.com>
2023-07-12 07:27:13 +0700

    Clean Master branch

--- Lab01/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace Lab01.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
