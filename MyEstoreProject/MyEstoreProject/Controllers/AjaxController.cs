638dc4d6fda476a3d0ae9e11b517006e7c125b78
Hien Luong <hyhien@gmail.com>
2023-08-23 10:51:44 +0700

    ThongKe

--- MyEstoreProject/MyEstoreProject/Controllers/AjaxController.cs
+++ MyEstoreProject/MyEstoreProject/Controllers/AjaxController.cs
@@ -2,6 +2,7 @@
 using Microsoft.EntityFrameworkCore;
 using MyEstoreProject.Data;
 using MyEstoreProject.Models;
+using static Microsoft.Extensions.Logging.EventSource.LoggingEventSource;
 
 namespace MyEstoreProject.Controllers
 {
@@ -39,7 +40,42 @@ namespace MyEstoreProject.Controllers
                 dsHangHoa = dsHangHoa.Where(hh => hh.TenHh.Contains(keyword));
             }
 
-            var data = dsHangHoa.Select(hh => new KetQuaTimKiemVM {
+            var data = dsHangHoa.Select(hh => new KetQuaTimKiemVM
+            {
+                MaHh = hh.MaHh,
+                TenHh = hh.TenHh,
+                DonGia = hh.DonGia.Value,
+                NgaySX = hh.NgaySx,
+                Loai = hh.MaLoaiNavigation.TenLoai
+            }).ToList();
+            return PartialView("TimKiemPartial", data);
+        }
+
+
+        [HttpGet]
+        public IActionResult TimKiem()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult TimKiem(string? name, double? fromPrice, double? toPrice)
+        {
+            var dsHangHoa = _context.HangHoas.Include(hh => hh.MaLoaiNavigation).AsQueryable();
+            if (name != null)
+            {
+                dsHangHoa = dsHangHoa.Where(hh => hh.TenHh.Contains(name));
+            }
+            if (fromPrice != null)
+            {
+                dsHangHoa = dsHangHoa.Where(hh => hh.DonGia.Value >= fromPrice);
+            }
+            if (toPrice != null)
+            {
+                dsHangHoa = dsHangHoa.Where(hh => hh.DonGia.Value <= toPrice);
+            }
+            var data = dsHangHoa.Select(hh => new KetQuaTimKiemVM
+            {
                 MaHh = hh.MaHh,
                 TenHh = hh.TenHh,
                 DonGia = hh.DonGia.Value,
---
+++ MyEstoreProject/MyEstoreProject/Controllers/ThongKeController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MyEstoreProject.Data;
+
+namespace MyEstoreProject.Controllers
+{
+    public class ThongKeController : Controller
+    {
+        private readonly MyeStoreContext _ctx;
+
+        public ThongKeController(MyeStoreContext ctx)
+        {
+            _ctx = ctx;
+        }
+        public IActionResult ThongKe()
+        {
+            var data = _ctx.ChiTietHds
+                .GroupBy(cthd => new
+                {
+                    cthd.MaHhNavigation.MaLoaiNavigation.TenLoai,
+                    cthd.MaHhNavigation.MaNccNavigation.TenCongTy
+                })
+                .Select(cthd => new
+                {
+                    cthd.Key.TenLoai,
+                    cthd.Key.TenCongTy,
+                    TongGiaTri = cthd.Sum(ct => ct.SoLuong * ct.DonGia),
+                    GiaNN = cthd.Min(ct => ct.DonGia),
+                    GiaLN = cthd.Max(ct => ct.DonGia),
+                });
+            return Json(data);
+        }
+    }
+}
